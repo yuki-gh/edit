5a57f28ed1eacd1d880e739538b71cb96276d672
Takayuki Ito <yuki@euc.jp>
2026-05-02 19:19:33 +0900

    fix

--- test.sh
+++ test.sh
@@ -19,91 +19,91 @@ fi
 case $1 in
 Dockerfile|dockerfile)
 	# Dockerfiles
-	find ../dockerfile-examples -name Dockerfile | while read -rf; do
+	find ../dockerfile-examples -name Dockerfile | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 properties)
 	# .properties
-	find ../JMeter-unofficial-ja -name '*.properties' | while read -rf; do
+	find ../JMeter-unofficial-ja -name '*.properties' | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 po)
 	# .po
-	find ../MonoDevelop-unofficial-ja -name '*.po' | while read -rf; do
+	find ../MonoDevelop-unofficial-ja -name '*.po' | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 config|conf)
 	# *config
-	find .. -name highlighting-tests -prune -o -name '*config' -print -o -name '*.ini' -print | while read -rf; do
+	find .. -name highlighting-tests -prune -o -name '*config' -print -o -name '*.ini' -print | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 xml|html)
 	# .xml/.html
-	find .. -name highlighting-tests -prune -o -name '*.xml' -print -o -name '*.html' -print -o -name '*.htm' -print -o -name '*.csproj' -print | while read -rf; do
+	find .. -name highlighting-tests -prune -o -name '*.xml' -print -o -name '*.html' -print -o -name '*.htm' -print -o -name '*.csproj' -print | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 rb|ruby)
 	# .rb
-	find .. -name '*.rb' | while read -rf; do
+	find .. -name '*.rb' | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 sh)
 	# .sh
-	find .. -name highlighting-tests -prune -o -name '*.sh' -print | while read -rf; do
+	find .. -name highlighting-tests -prune -o -name '*.sh' -print | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 md)
 	# .md
-	find .. -name highlighting-tests -prune -o -name '*.md' -print | while read -rf; do
+	find .. -name highlighting-tests -prune -o -name '*.md' -print | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 yaml|yml)
 	# .yaml/.yml
-	find .. -name highlighting-tests -prune -o -name '*.yaml' -print -o -name '*.yml' -print | while read -rf; do
+	find .. -name highlighting-tests -prune -o -name '*.yaml' -print -o -name '*.yml' -print | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 json)
 	# .json
-	find .. -name highlighting-tests -prune -o -name '*.json' -print | while read -rf; do
+	find .. -name highlighting-tests -prune -o -name '*.json' -print | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 py|python)
 	# .py
-	find .. -name highlighting-tests -prune -o -name '*.py' -print | while read -rf; do
+	find .. -name highlighting-tests -prune -o -name '*.py' -print | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 js|javascript)
 	# .js/.jsx
-	find .. -name highlighting-tests -prune -o -name '*.js' -print -o -name '*.jsx' -print | while read -rf; do
+	find .. -name highlighting-tests -prune -o -name '*.js' -print -o -name '*.jsx' -print | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
 
 ignore|gitignore)
 	# .gitignore
-	find .. -name highlighting-tests -prune -o -name '.gitignore' -print | while read -rf; do
+	find .. -name highlighting-tests -prune -o -name '.gitignore' -print | while read -r f; do
 		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
 	done
 	;;
