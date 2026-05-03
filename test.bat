37ee6fff3c1ce6abcf1512141b334a9b986f8eda
Takayuki ITO <20669043+yuki-gh@users.noreply.github.com>
2026-05-04 01:51:53 +0900

    pause

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 
 cd "%~dp0"
 chcp 65001
@@ -16,72 +16,84 @@ goto %1
 :bas
 for /r ..\basic-computer-games\00_Alternate_Languages %%f in (*.bas) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :sln
 for /r .. %%f in (*.sln) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :bat
 for /r .. %%f in (*.bat) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :cs
 for /r .. %%f in (*.cs) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :cpp
 for /r .. %%f in (*.cpp *.hpp) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :c
 for /r .. %%f in (*.c *.h) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :rs
 for /r .. %%f in (*.rs) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :dockerfile
 for /r ..\dockerfile-examples %%f in (Dockerfile*) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :properties
 for /r .. %%f in (*.properties) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :po
 for /r .. %%f in (*.po) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :config
 for /r .. %%f in (*config *.ini) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :toml
 for /r %%f in (*.toml) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
@@ -89,24 +101,28 @@ exit /b
 :html
 for /r .. %%f in (*.xml *.html *.htm *.csproj) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :rb
 for /r .. %%f in (*.rb) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :sh
 for /r .. %%f in (*.sh) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :md
 for /r .. %%f in (*.md) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
@@ -114,12 +130,14 @@ exit /b
 :yaml
 for /r .. %%f in (*.yml *.yaml) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :json
 for /r .. %%f in (*.json) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
@@ -127,6 +145,7 @@ exit /b
 :python
 for /r .. %%f in (*.py) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
@@ -134,6 +153,7 @@ exit /b
 :javascript
 for /r .. %%f in (*.js) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
@@ -141,11 +161,13 @@ exit /b
 :gitignore
 for /r .. %%f in (.gitignore*) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
 
 :lsh
 for %%f in (crates\lsh\definitions\*.lsh) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
 )
 exit /b
