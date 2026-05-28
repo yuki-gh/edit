8eb5d6ed1917a109c1103e17cc0a9aab26aa3807
Takayuki ITO <20669043+yuki-gh@users.noreply.github.com>
2026-05-28 18:13:20 +0900

    awk

--- test.sh
+++ test.sh
@@ -49,6 +49,13 @@ setup)
 	fi
 	;;
 
+awk)
+	# .awk
+	find ../protobuf/java ../binutils-2.41/gprof -name '*.awk' | sort | head | while read -r f; do
+		cargo run -p lsh-bin -- render --input "$f" crates/lsh/definitions 2>/dev/null | less -r
+	done
+	;;
+
 unity)
 	# .unity
 	find ../arfoundation-samples/Assets -name '*.unity' -o -name '*.prefab' -o -name '*.asset' | sort | head | while read -r f; do
