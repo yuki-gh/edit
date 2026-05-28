5791e750b4b58738ba5f2d1fd9190ffcf5bb3553
Takayuki ITO <20669043+yuki-gh@users.noreply.github.com>
2026-05-28 18:19:01 +0900

    tests

--- test.bat
+++ test.bat
@@ -13,6 +13,13 @@ if "%1"=="" (
 
 goto %1
 
+:awk
+for /r ..\protobuf\java %%f in (*.awk) do (
+	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
+	pause
+)
+exit /b
+
 :unity
 for /r ..\arfoundation-samples\Assets %%f in (*.unity *.prefab *.asset) do (
 	cargo run -p lsh-bin -- render --input "%%f" crates\lsh\definitions 2>nul | more
